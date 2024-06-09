6c6b409755f615c5857719ed5b6107c7afa6029d
Abdul Wahab <abdulwahab4099@gmail.com>
2024-06-09 12:55:51 +0500

    Class01(variables)

---
+++ class01/variables.js
@@ -0,0 +1,12 @@
+"use strict";
+//  Here we'll be learning the concept of variables in TypeScript.
+//  A variable is a container for storing data values.
+//  A variable is created the moment you first
+//  assign a value to it.
+//  A variable must be declared before it is used.
+var x = 10; // here it's a variable x whose type is number.
+var y = "Hello World"; // here it's a variable y whose type is string.
+var userName = "Abdul Wahab";
+console.log(userName);
+console.log(x);
+console.log(y);
