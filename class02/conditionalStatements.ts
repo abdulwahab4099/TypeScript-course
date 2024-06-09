544c01cb880edd2c3bba5616ead031cef1fd2f0f
Abdul Wahab <abdulwahab4099@gmail.com>
2024-06-09 12:56:36 +0500

    class-02(Data-Types)

---
+++ class02/conditionalStatements.ts
@@ -0,0 +1,18 @@
+
+
+if(false) {
+console.log("Line1");
+console.log("Line2");
+console.log("Line3");
+console.log("Line4");
+console.log("Line5");
+}
+
+if(true) {
+console.log("Line6");
+console.log("Line7");
+console.log("Line8");
+console.log("Line9");
+console.log("Line10");
+
+}
---
+++ class02/index.js
@@ -0,0 +1,91 @@
+"use strict";
+// Uncomment below code first if you want to run it...
+/*
+
+console.log("Abdul Wahab")
+
+// camel case
+var myNameIs="Abdul Wahab"
+
+console.log(myNameIs)
+
+// snake case
+var my_name_is="Abdul Wahab"
+
+console.log(my_name_is)
+
+// pascal case
+var MyNameIs="Abdul Wahab"
+
+console.log(MyNameIs)
+
+// * Comments:
+
+//  Here we'll be learning the concept of comments in TypeScript.
+
+*/
+// var newVar:number=22.2;
+// var aNewVar=null
+// console.log(aNewVar)
+/*
+var x1=34;
+var x2=`ABC`;
+var x3=true;
+var x4=null;
+var x5 = x1==x2;
+
+console.log(typeof x1)
+
+console.log(typeof x2)
+
+console.log(typeof x3)
+
+console.log(typeof x4)
+
+console.log(typeof x5)
+
+*/
+var n1 = 5;
+var n2 = 10;
+var n3 = 15;
+var n4 = 20;
+var result = n1 + n2 - n3 * n4 / n2;
+console.log(result);
+/*
+// * Operators:
+
+//  Here we'll be learning the concept of operators in TypeScript.
+//  Comparison Operators:
+
+
+//  Arithmetic Operators:
+
+
+//  Assignment Operators:
+
+//  Bitwise Operators:
+
+//  Logical Operators:
+
+//  Ternary Operators:
+
+//  Nullish Coalescing Operator:
+*/
+/*
+
+T && T => T
+T && F => F
+F && T => F
+F && F => F
+
+
+T || T => T
+T || F => T
+F || T => T
+F || F => F
+
+*/
+var output = 5 == 5 && 2 == 2;
+console.log(output);
+var output2 = 5 == 5 || 2 == 2;
+console.log(output2);
